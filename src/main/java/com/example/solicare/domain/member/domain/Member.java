957670333b0ed173656d8db40c22fd744de518ad
heehoonhong <gmlgns0522@naver.com>
2025-07-26 22:49:02 +0900

    feat

--- src/main/java/com/example/solicare/domain/member/domain/Member.java
+++ src/main/java/com/example/solicare/domain/member/domain/Member.java
@@ -1,4 +1,31 @@
 package com.example.solicare.domain.member.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String elderlyName;
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Role role = Role.USER;
 }
--- src/main/java/com/example/solicare/domain/member/infrastructure/MemberRepository.java
+++ src/main/java/com/example/solicare/domain/member/infrastructure/MemberRepository.java
@@ -1,4 +1,10 @@
 package com.example.solicare.domain.member.infrastructure;
 
-public class MemberRepository {
+import com.example.solicare.domain.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByPhoneNumber(String phoneNumber);
 }
