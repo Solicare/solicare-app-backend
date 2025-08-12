1d172d00586d18b2cfb7116cf6436470494d3bf0
YEONGCHANG YUN <94744740+YeongT@users.noreply.github.com>
2025-08-12 21:51:13 +0900

    feat: update CORS configuration to restrict allowed origins for enhanced security

--- src/main/java/com/example/solicare/global/config/SecurityConfig.java
+++ src/main/java/com/example/solicare/global/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.example.solicare.global.auth.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +13,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -52,7 +52,12 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Collections.singletonList("*")); // 모든 Origin 허용
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost",
+                "http://localhost:*",
+                "http://*.solicare.kro.kr",
+                "https://*.solicare.kro.kr"
+        )); // localhost와 *.solicare.kro.kr 허용
         configuration.setAllowedMethods(Collections.singletonList("*"));        // 모든 HTTP 메서드 허용
         configuration.setAllowedHeaders(Collections.singletonList("*"));        // 모든 Header 허용
         configuration.setAllowCredentials(true);                                // 인증 정보 포함 허용
